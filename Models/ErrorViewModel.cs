9e9892902993dc1732ad9fa21c4903667e3f5ade
erhanbasaran <erha0045@stud.kea.dk>
2023-04-13 09:08:16 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GithubActionsTestApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
